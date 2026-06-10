2c823d92e7e955fe93ca42417a7e59305107a71d
devRao <dev.raorizwan@gmail.com>
2026-06-10 22:59:46 +0500

    logic error

--- 01 Python Basic Exercise for Beginners/exercise25.py
+++ 01 Python Basic Exercise for Beginners/exercise25.py
@@ -4,10 +4,12 @@
 # Given Input: year = 2024
 # Expected Output: 2024 is a leap year
 
+
+# A year is a leap year if it is divisible by 4. However, if it is divisible by 100, it is NOT a leap year UNLESS it is also divisible by 400.
 def find_leapyear(current_year):
-    condition_1 = current_year % 4 == 0 and current_year % 100
-    condition_2 = current_year % 400 != 0
-    return f"{current_year} is a leap year" if condition_1 and condition_2 else f"{current_year} is not a leap year"
+    condition_1 = current_year % 4 == 0 and current_year % 100 != 0
+    condition_2 = current_year % 400 == 0
+    return f"{current_year} is a leap year" if condition_1 or condition_2 else f"{current_year} is not a leap year"
 
 
 if __name__ == "__main__" :
