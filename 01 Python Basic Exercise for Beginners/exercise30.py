284ad5c719be86ca0433a01425f202e0bec23f29
devRao <dev.raorizwan@gmail.com>
2026-06-13 01:28:58 +0500

    30

---
+++ 01 Python Basic Exercise for Beginners/exercise30.py
@@ -0,0 +1,16 @@
+#  Write a program that counts how many times each word appears 
+# in a given paragraph and stores these counts in a dictionary.
+# Given Input: text = "apple banana apple cherry banana apple"
+# Expected Output: {'apple': 3, 'banana': 2, 'cherry': 1}
+
+text = "apple banana apple cherry banana apple"
+words = text.split()
+frequency = {}
+for word in words :
+    if word in frequency :
+        frequency[word] += 1
+    else :
+        frequency[word] = 1
+        
+
+print(frequency)
