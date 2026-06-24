15faf2c766fd132edfaba1d152e143c27357fc22
devRao <dev.raorizwan@gmail.com>
2026-06-24 13:56:43 +0500

    1

---
+++ 02 Python Loops Exercises/exercise01.py
@@ -0,0 +1,14 @@
+#  Write a program to print the first 10 natural numbers using a while loop. Each number should be printed on a new line.
+# Given Input: None (The range is fixed from 1 to 10).
+# Expected Output:
+# 1
+# 2
+# 3
+# ...
+# 10
+
+i = 1
+while i <= 10 :
+    print(i)
+    i += 1
+    
