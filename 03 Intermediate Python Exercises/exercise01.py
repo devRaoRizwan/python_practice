6f8bbad9e1f1d77c319755cf7231aff11ef4e8fb
devRao <dev.raorizwan@gmail.com>
2026-07-05 14:16:18 +0500

    ex01

--- 03 Intermediate Python Exercises/exercise01.py
+++ 03 Intermediate Python Exercises/exercise01.py
@@ -3,9 +3,6 @@
 # Given Input: words = ["apple", "bat", "cherry", "dog", "elderberry"]
 # Expected Output: ['APPLE', 'CHERRY', 'ELDERBERRY']
 
-def string_shortner(words):
-    return [word.upper() for word in words if len(word) > 4]
-
-if __name__ == "__main__":
-    words = ["apple", "bat", "cherry", "dog", "elderberry"]
-    print(string_shortner(words))
+words = ["apple", "bat", "cherry", "dog", "elderberry"]
+result = [word.upper() for word in words if len(word) > 4]
+print(result)
